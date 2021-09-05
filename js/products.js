69672d29a2cecdccddc00140421263b5c113f182
Guillermo van der laan <70448664+guillermovanderlaan@users.noreply.github.com>
2021-09-05 14:24:19 -0300

    Entrega 2
    jovenes a programar

---
+++ js/products.js
@@ -0,0 +1,147 @@
+// Variables que van a determinar los criterios por los cuales se van a ordenar los filtros, tanto de precios como de relevancia.//
+
+const ORDER_ASC_BY_COST = "MINMAX";
+const ORDER_DESC_BY_COST = "MAXMIN";
+const ORDER_BY_SOLD_COUNT = "Cant.";
+var currentProductsArray = [];
+var currentSortCriteria = undefined;
+var minCost = undefined;
+var maxCost = undefined;
+
+
+function sortProducts(criteria, array) {
+    let result = [];
+    if (criteria === ORDER_ASC_BY_COST) {
+        result = array.sort(function (a, b) {
+            let aCost = parseInt(a.cost);
+            let bCost = parseInt(b.cost);
+
+            if (aCost < bCost) { return -1; }
+            if (aCost > bCost) { return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_DESC_BY_COST) {
+        result = array.sort(function (a, b) {
+            let aCost = parseInt(a.cost);
+            let bCost = parseInt(b.cost);
+
+            if (aCost > bCost) { return -1; }
+            if (aCost < bCost) { return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_BY_SOLD_COUNT) {
+        result = array.sort(function (a, b) {
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
+
+            if (aCount > bCount) { return -1; }
+            if (aCount < bCount) { return 1; }
+            return 0;
+        });
+    }
+
+    return result;
+}
+
+
+function showProductsList() {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < currentProductsArray.length; i++) {
+        let products = currentProductsArray[i];
+
+        
+        if (((minCost == undefined) || (minCost != undefined && parseInt(products.cost) >= minCost)) &&
+            ((maxCost == undefined) || (maxCost != undefined && parseInt(products.cost) <= maxCost))) {
+
+            htmlContentToAppend += `
+            <div class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">`+ products.name + `</h4>
+                                <small class="text-muted">` + products.soldCount + ` artículos</small>
+                            </div>
+                            <p>` + products.description + `</p>
+                            <p class="text-muted">`+ products.currency + " " + products.cost + `  </p>
+                        </div>
+                    </div>
+                </div>
+                `
+            }
+
+                document.getElementById("products").innerHTML = htmlContentToAppend;
+    }
+}
+
+
+function sortAndShowProducts(sortCriteria, productsArray) {
+    currentSortCriteria = sortCriteria; //Se le da un criterio a currentSortCriteria para que se muestre los productos segun el criterio seleccionado.
+
+    if (productsArray != undefined) {
+        currentProductsArray = productsArray; //Si productsArray es distinto a undefined, nos devolverá una lista.
+    }
+
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+
+
+    showProductsList();
+}
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            sortAndShowProducts(ORDER_ASC_BY_COST, resultObj.data);
+        }
+    });
+
+    document.getElementById("sortAsc").addEventListener("click", function () {
+        sortAndShowProducts(ORDER_ASC_BY_COST);
+    });
+
+    document.getElementById("sortDesc").addEventListener("click", function () {
+        sortAndShowProducts(ORDER_DESC_BY_COST);
+    });
+
+    document.getElementById("sortByCount").addEventListener("click", function () {
+        sortAndShowProducts(ORDER_BY_SOLD_COUNT);
+    });
+
+    document.getElementById("clearRangeFilter").addEventListener("click", function () {
+        document.getElementById("rangeFilterCostMin").value = "";
+        document.getElementById("rangeFilterCostMax").value = "";
+
+        minCost = undefined;
+        maxCost = undefined;
+
+        showProductsList();
+    });
+
+    document.getElementById("rangeFilterCost").addEventListener("click", function () {
+        //Se obtiene tanto el maximo valor como el minimo valor posible de los intervalos a modo de que filtre y muestre en pantalla el rango de valores que el usuario pone en pantalla.
+        minCost = document.getElementById("rangeFilterCostMin").value;
+        maxCost = document.getElementById("rangeFilterCostMax").value;
+
+        if ((minCost != undefined) && (minCost != "") && (parseInt(minCost)) >= 0) {
+            minCost = parseInt(minCost);
+        }
+        else {
+            minCost = undefined;
+        }
+
+        if ((maxCost != undefined) && (maxCost != "") && (parseInt(maxCost)) >= 0) {
+            maxCost = parseInt(maxCost);
+        }
+        else {
+            maxCost = undefined;
+        }
+
+        showProductsList();
+    });
+});
